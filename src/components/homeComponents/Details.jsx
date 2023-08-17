a3053448b73b0db50713b40fa514ff95b6876071
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-17 23:00:26 +0800

    clickable social links for facebook and jobstreet

--- src/components/homeComponents/Details.jsx
+++ src/components/homeComponents/Details.jsx
@@ -9,8 +9,8 @@ function Details() {
             <div>Hi, I'm Aaron Joseph Carillo. <br/> A passionate Web Developer <br/> based in Pasig, Philippines. </div>
         </div>
         <div className="details-social">
-            <img src={facebookLogo} className='details-facebookLogo'/>
-            <img src={jobstreetLogo} className='details-jobstreetLogo'/>
+            <img src={facebookLogo} className='details-facebookLogo' href="https://www.facebook.com/aaronjoseph.carillo/"/>
+            <img src={jobstreetLogo} className='details-jobstreetLogo' href="https://myjobstreet.jobstreet.com.ph/resume/attachment.php?tk=1&rid=11963376932"/>
         </div>
         </>
     )
